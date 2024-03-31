63d282845e05fa13af5206425e8a8c2262a0c9e1
ErwinAdriel <adrielquispe87@gmail.com>
2024-03-31 10:22:28 -0300

    Instalando libreria Big.js y armando estructura de las operaciones

---
+++ src/logica/operaciones.js
@@ -0,0 +1,15 @@
+import Big from "big.js";
+import operate from "./operate";
+import isNumber from "./inNumber";
+
+export default function operaciones(estado, nombreDeBoton){
+
+    if(nombreDeBoton === "AC"){
+        return{
+            total: null,
+            siguiente: null,
+            operador: null
+        }
+    }
+
+}
---
+++ src/logica/operate.js
@@ -0,0 +1,20 @@
+// Libreria Big.js para manejar operaciones aritmeticas
+import Big from "big.js";
+
+export default function operate(numeroUno, numeroDos, operador){
+    const uno = Big(numeroUno || "0")
+    const dos = Big(numeroDos || (operador === "/" || operador === "*" ? "1" : "0"))
+
+    if (operador === "+") return uno.plus(dos).toString()
+
+    if (operador === "-") return uno.minus(dos).toString()
+
+    if (operador === "*") return uno.times(dos).toString()
+
+    if (operador === "/") if (dos == "0"){
+        alert("ERROR")
+    }else {
+        return uno.div(dos).toString()
+    }
+
+} 
